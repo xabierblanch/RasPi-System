5cf10cd8983242ea4332d15eca6fbdd9d2500b73
Xabier Blanch Górriz <37353398+xabierblanch@users.noreply.github.com>
2022-12-06 10:28:51 +0100

    Delete afterStartup.sh

--- afterStartup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# file: afterStartup.sh
-#
-# This script will be executed in background after Witty Pi 3 gets initialized.
-# If you want to run your commands after boot, you can place them here.
-#
-#python3 /home/pi/raspi.py >> /media/usb/raspi.log
-python3 /home/pi/raspi.py
