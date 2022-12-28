c86d4a6d6e6e4ca4e2ce9c19d6b7faea6b1c904f
Xabier Blanch Górriz <37353398+xabierblanch@users.noreply.github.com>
2022-12-29 00:55:15 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
+#run main script
 python3 /home/pi/Scripts/main_v1.1.py >> /home/pi/logs/script.log
 
-python3 /home/pi/Scripts/upload_logs.py
+#move and upload logs
+python3 /home/pi/Scripts/logs.py
 
+#shutdown RPi
 gpio -g mode 4 out
 gpio -g write 4 0
-
-# Final de l'escript
-
-
-
