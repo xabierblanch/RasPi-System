fb771bac40b2595bd979f42a4032938648cd1b16
Xabier Blanch Górriz <37353398+xabierblanch@users.noreply.github.com>
2022-12-29 00:47:04 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-sudo sh /home/pi/wittyPi/temp.sh
+python3 /home/pi/Scripts/UB_PUIG_0.4.py >> /home/pi/logs/script.log
 
-sudo python3 /home/pi/Scripts/UB_PUIG_0.4.py >> /home/pi/logs/log_Script_RasPi04.log
-
-sh /home/pi/Scripts/logs_RasPi.sh
-
-sudo python3 /home/pi/Scripts/upload_logs.py
+python3 /home/pi/Scripts/upload_logs.py
 
 gpio -g mode 4 out
 gpio -g write 4 0
