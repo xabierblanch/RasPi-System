f21022f5a9525c950a98c8e73106f963ced70e7f
Xabier Blanch Górriz <37353398+xabierblanch@users.noreply.github.com>
2022-12-29 00:28:02 +0100

    Update UB_PUIG_0.4.py

--- UB_PUIG_0.4.py
+++ UB_PUIG_0.4.py
@@ -45,15 +45,15 @@ def setup_camera():
 
 		if camera.revision == 'imx477':
 			camera.resolution = (4056,3040)
-			print(get_time() + f'Camera HQ detected. Resolution: {camera.resolution}')
+			print(f'Camera HQ detected. Resolution: {camera.resolution}')
 		elif camera.revision == 'imx219':
 			camera.resolution = (3280, 2464)
-			print(get_time() + f'Camera V2 detected. Resolution: {camera.resolution}')
+			print(f'Camera V2 detected. Resolution: {camera.resolution}')
 		else:
 			camera.resolution = (2592, 1944)
-			print(get_time() + f'Camera V1 detected. Resolution: {camera.resolution}')
+			print(f'Camera V1 detected. Resolution: {camera.resolution}')
 	except:
-		print(get_time() + 'ERROR: Camera module not found')
+		print('ERROR: Camera module not found')
 	return camera
 
 def paths():
@@ -61,16 +61,16 @@ def paths():
 	os.makedirs(path_backup, exist_ok=True)
 	print(f"The {path_temp}  + and {path_backup} were succesfuly created")
 
-def image_capture(count, camera):	
+def image_capture(count, camera, path_temp):	
 	date = datetime.datetime.now().strftime('%Y%m%d_%H%M')
 	try:
-		save_file = os.path.join(path, ID+date+'_'+str(count)+'.jpg')
+		save_file = os.path.join(path_temp, ID+date+'_'+str(count)+'.jpg')
 		camera.capture(save_file, format='jpeg', quality=100)
-		print(get_time() + 'Image: ' + date + '_' + str(count) + ' successfully captured')
+		print('Image: ' + date + '_' + str(count) + ' successfully captured')
 	except:
-		print(get_time() + 'ERROR: Image capture fail')		
+		print('ERROR: Image capture fail')		
 
-def dropbox_upload(token):
+def dropbox_upload(token, path_temp, path_backup):
 	if os.listdir(path_temp) == []:
 		print(f"There are no files in the {path_temp} folder to upload to Dropbox")
 	else:
@@ -79,22 +79,22 @@ def dropbox_upload(token):
 			try:
 				dbx = dropbox.Dropbox(token)
 				res=dbx.files_upload(f.read(),'/' + file)
-				shutil.move(os.path.join(path_temp,file), os.path.join(path_backup,file))
+				shutil.move(os.path.join(path_temp, file), os.path.join(path_backup, file))
 				print(f'File {res.name} successfully uploaded to Dropbox and backup stored')
 			except dropbox.exceptions.ApiError as err:
 				print('*** API error', err)
 				return none
 		return res
 
-def borrar_fitxers():
-	data_actual = time.time()
-	eliminats = 0
-	for fitxer in os.listdir(path_final):
-		data_fitxer = os.path.getmtime(os.path.join(path_backup,file))
-		if ((data_actual - data_fitxer) / (24*3600)) >= 30:
-			os.unlink(os.path.join(path_backup,file))
-			eliminats = eliminats + 1
-	print(f"{str(eliminats)} files have been deleted due to exceeding the maximum number of days of backup")
+def delete_backup(path_backup):
+	today = time.time()
+	deleted = 0
+	for file in os.listdir(path_backup):
+		data_file = os.path.getmtime(os.path.join(path_backup, file))
+		if ((today - data_file) / (24*3600)) >= 30:
+			os.unlink(os.path.join(path_backup, file))
+			deleted = deleted + 1
+	print(f"{str(deleted)} files have been deleted due to exceeding the maximum number of days of backup")
 
 # MAIN CODE
 print(datetime.datetime.now().strftime("Time: %H:%M Date: %d/%m/%Y"))
@@ -104,16 +104,16 @@ path_backup = "/home/pi/" + ID + "backup"
 
 camera = setup_camera()
 paths()
-borrar_fitxers()
+delete_backup(path_backup)
 count=1
 for count in range (1, burst_num+1):
-	image_capture(count, camera)
+	image_capture(count, camera, path_temp)
 	sleep(1)
 	count=count + 1
 
 sleep(1)
 try:
-	dropbox_upload(token)
+	dropbox_upload(token, path_temp, path_backup)
 except:
 	print("Error DROPBOX - Error in the transfer of files to Dropbox")
 print()
